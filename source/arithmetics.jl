2604246ed1558fc3f45ce38c3d995b1f7768946a
Henrique Marques <77990915+html-3@users.noreply.github.com>
2022-09-12 21:43:41 -0300

    ➕ ADD e HADD adicionados

---
+++ source/arithmetics.jl
@@ -0,0 +1,52 @@
+include("gates.jl")
+
+"""Half-Adder Component
+
+  ----
+
+  s = x ⊕ y
+
+  c = x ∙ y
+
+  ----
+
+  | x | y | s | c |
+  |:-:|:-:|:-:|:-:|
+  | 0 | 0 | 0 | 0 |
+  | 0 | 1 | 1 | 0 |
+  | 1 | 0 | 1 | 0 |
+  | 1 | 1 | 0 | 1 |
+"""
+function HADD(x::BitVector, y::BitVector)
+  s = XOR(x,y)
+  c = AND(x,y)
+  return s, c
+end
+
+"""Full-Adder Component
+
+  ----
+
+  s = x ⊕ y ⊕ c_in
+
+  c_out = x ∙ y + (x ⊕ y) ∙ c_in 
+  
+  ----
+
+  | c_in | x | y | c_out | s |
+  |:----:|:-:|:-:|:-----:|:-:|
+  |  0   | 0 | 0 |   0   | 0 |
+  |  0   | 0 | 1 |   0   | 1 |
+  |  0   | 1 | 0 |   0   | 1 |
+  |  0   | 1 | 1 |   1   | 1 |
+  |  1   | 0 | 0 |   0   | 1 |
+  |  1   | 0 | 1 |   1   | 0 |
+  |  1   | 1 | 0 |   1   | 0 |
+  |  1   | 1 | 1 |   1   | 1 |
+"""
+function ADD(x::BitVector, y::BitVector)
+  z = XOR(x,y)
+  s = XOR(z,c)
+  c = OR(AND(x,y),AND(z,c))
+  return s, c
+end
